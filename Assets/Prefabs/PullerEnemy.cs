bf293fe57cc305c3b094dec302381e23bb14b556
SamTech37 <87892685+SamTech37@users.noreply.github.com>
2022-07-10 00:05:23 +0800

    changed editor to VScode

--- Assets/Prefabs/PullerEnemy.cs
+++ Assets/Prefabs/PullerEnemy.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class PullerEnemy : FollowerEnemy
 {
-
-   
-    
     new private void OnCollisionEnter2D(Collision2D other)
     {
          if (other.collider.gameObject.GetComponent<PlayerMovement>())
          {
             Vector2 push = transform.position - target.position  ; //PULL the player
-            other.rigidbody.AddForce(push.normalized * 5000f);
+            other.rigidbody.AddForce(push.normalized * 5000f,ForceMode2D.Force);
 
             Death();
          }
